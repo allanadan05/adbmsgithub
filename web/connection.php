9f024d93690e328a52ff7a80004c62d704d7b013
allanadan05 <allanadan1999@gmail.com>
2019-11-30 05:42:29 -0800

    cut web

--- web/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-mysql_connect("localhost","root","") or die ("server not connected!");
-mysql_select_db("STEVEN_DATABASE") or die ("DATABASE NOT FOUND!");
-
-?>
--- web/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("connection.php");
-
-$ID=$_GET['ID'];
-mysql_query("DELETE FROM STEVEN_TABLE WHERE ID=$ID");
-
-
-header("refresh:0.01; url=http://localhost/web/display.php");
-?>
